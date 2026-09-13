586d5cb2c8d6979b1e0bd663309753b52bfb1f6b
Josue Espino <info@fencemastersdfw.com>
2026-09-12 20:08:18 -0500

    Create homepage route and display Fence Project records

--- index.js
+++ index.js
@@ -14,6 +14,28 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const fenceProjects =
+        'https://api.hubapi.com/crm/v3/objects/2-268272831?properties=name,fence_type,city';
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const resp = await axios.get(fenceProjects, { headers });
+        const data = resp.data.results;
+
+        res.render('homepage', {
+            title: 'Fence Projects | HubSpot Practicum',
+            data
+        });
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Error retrieving Fence Project records.');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
